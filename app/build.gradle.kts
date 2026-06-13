ae17096e18baea07129efedbdf88ef55a34fbfc9
Newo Ether <newoether2000@gmail.com>
2026-06-13 09:50:14 +0800

    chore: bump version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.newoether.agora"
         minSdk = 24
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.3.0"
+        versionCode = 19
+        versionName = "1.3.1"
 
 
         ndk {
